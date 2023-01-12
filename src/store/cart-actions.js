0d87e3e5d2fe0283692b97da15edebac87f25533
Cypher890 <abdulsuhaib07@gmail.com>
2023-01-12 18:20:32 +0530

    finalised the fetching action

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,5 +1,5 @@
-import { cartActions } from "./cart-slice";
-import { uiActions } from "./ui-slice";
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
 
 export const fetchCartData = () => {
   return async (dispatch) => {
@@ -7,22 +7,30 @@ export const fetchCartData = () => {
       const response = await fetch(
         "https://reactreduxcart-159cd-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
       );
+
       if (!response.ok) {
-        throw new Error("Could not fetch cart data!");
+        throw new Error('Could not fetch cart data!');
       }
+
       const data = await response.json();
 
       return data;
     };
+
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "fetching cart data failed!",
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
         })
       );
     }
@@ -33,9 +41,9 @@ export const sendCartData = (cart) => {
   return async (dispatch) => {
     dispatch(
       uiActions.showNotification({
-        status: "pending",
-        title: "sending...",
-        message: "Sending cart data!",
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
       })
     );
 
@@ -43,33 +51,35 @@ export const sendCartData = (cart) => {
       const response = await fetch(
         "https://reactreduxcart-159cd-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
         {
-          method: "PUT",
+          method: 'PUT',
           body: JSON.stringify({
             items: cart.items,
             totalQuantity: cart.totalQuantity,
           }),
         }
       );
+
       if (!response.ok) {
-        throw new Error("sending data failed");
+        throw new Error('Sending cart data failed.');
       }
     };
 
     try {
       await sendRequest();
+
       dispatch(
         uiActions.showNotification({
-          status: "success",
-          title: "Success",
-          message: "Sent cart data successfully!",
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
         })
       );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
         })
       );
     }
